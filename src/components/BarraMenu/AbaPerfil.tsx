7b5f7dacb4eb2b5ae480fdc2524e7a76cca15837
yurisantosdev <yuri.santos.cco@unochapeco.edu.br>
2025-07-01 15:42:28 -0300

    321

--- src/components/BarraMenu/AbaPerfil.tsx
+++ src/components/BarraMenu/AbaPerfil.tsx
@@ -130,7 +130,7 @@ export default function AbaPerfil() {
                 )}
 
                 {/* Botão troca de senha e endereço */}
-                <div className="mt-14 flex justify-center items-center gap-2">
+                <div className="mt-14 md:flex md:justify-center md:items-center gap-2">
                   {/* Botão trocar senha */}
                   <div
                     className="tooltip tooltip-bottom w-full"
@@ -142,13 +142,13 @@ export default function AbaPerfil() {
                         CLickLabel('modalTrocarSenha')
                       }}
                       iconLeft={<LockKey size={20} />}
-                      className="w-full m-auto bg-blue-1000 hover:bg-blue-1000/80 active:bg-blue-1000 text-white rounded-lg transition-colors duration-300 shadow-md hover:shadow-lg flex items-center gap-2 p-3"
+                      className="w-full m-auto bg-blue-1000 hover:bg-blue-1000/80 active:bg-blue-1000 text-white rounded-lg transition-colors duration-300 shadow-md hover:shadow-lg flex items-center gap-2 p-3 truncate"
                     />
                   </div>
 
                   {/* Botão trocar de endereço */}
                   <div
-                    className="tooltip tooltip-bottom w-full"
+                    className="tooltip tooltip-bottom w-full md:mt-0 mt-4"
                     data-tip="Troca de endereço">
                     <Button
                       iconLeft={<MapTrifold size={20} />}
@@ -170,7 +170,7 @@ export default function AbaPerfil() {
                         CLickLabel('modalTrocarEndereco')
                       }}
                       title="Trocar endereço"
-                      className="w-full m-auto bg-orange-1000 hover:bg-orange-1000/80 active:bg-orange-1000 text-white rounded-lg transition-colors duration-300 shadow-md hover:shadow-lg flex items-center gap-2 p-3"
+                      className="w-full m-auto bg-orange-1000 hover:bg-orange-1000/80 active:bg-orange-1000 text-white rounded-lg transition-colors duration-300 shadow-md hover:shadow-lg flex items-center gap-2 p-3 truncate"
                     />
                   </div>
                 </div>
