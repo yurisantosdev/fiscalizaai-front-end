290e06d2a0c85ce801aad06ac772b447405a0e74
yurisantosdev <yuri.santos.cco@unochapeco.edu.br>
2025-07-10 15:53:09 -0300

    312321

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -39,7 +39,7 @@ export default function Textarea({
             className={`${icon ? 'px-11' : 'px-5'} py-2 resize-none ${
               error
                 ? 'bg-red-400 border border-red-600 placeholder:text-red-600 focus:outline-red-500'
-                : 'bg-gray-50 border border-gray-300 placeholder:text-gray-300 focus:bg-gray-100 focus:outline-blue-1000'
+                : 'bg-white border border-gray-300 placeholder:text-gray-300 focus:bg-gray-100 focus:outline-blue-1000'
             }  rounded-md w-full text-black focus:border-none ${
               props.className ?? ''
             }`}
